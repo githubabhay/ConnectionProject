2929a02fe4a37d9208d5fe02748cf25a061a7284
abhay <abhaykesi@gmail.com>
2018-10-29 19:17:39 +0530

    Initial checkin

---
+++ AutoCadPlotConnection/AutoCadPlotConnection/Module1.vb
@@ -0,0 +1,13 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim AutoCadObj As Object
+        Dim AutoCadPath As String
+
+        AutoCadObj = GetObject(, "AutoCAD.Application")
+        AutoCadPath = AutoCadObj.Path
+        AutoCadObj.LoadDVB(My.Application.Info.DirectoryPath & "\Test.dvb")
+        AutoCadObj.RunMacro("PlotTest")
+    End Sub
+
+End Module
---
+++ WindowsApp1/WindowsApp1/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    ''' <summary>
+    ''' 
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim AutoCadObj As Object
+        Dim AutoCadPath As String
+
+        AutoCadObj = GetObject(, "AutoCAD.Application")
+        AutoCadPath = AutoCadObj.Path
+        AutoCadObj.LoadDVB(My.Application.Info.DirectoryPath & "\Test.dvb")
+        AutoCadObj.RunMacro("PlotTest")
+    End Sub
+End Class
